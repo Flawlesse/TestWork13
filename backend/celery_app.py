dfea313a8f59260375a008a0baf67405e265f18c
flawlesse <lavrenovichae@gmail.com>
2025-09-17 16:25:56 +0300

    initial proj layout

---
+++ backend/celery_app.py
@@ -0,0 +1,10 @@
+from celery import Celery
+import os
+
+
+app = Celery("backend")
+app.config_from_object("celeryconfig")
+
+# we need to do that in order to actually run the modules
+# where the tasks are registered
+import tasks  # NOQA
