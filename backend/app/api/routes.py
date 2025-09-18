33cbc0cd3e485549871cb0512395506f20ef2479
flawlesse <lavrenovichae@gmail.com>
2025-09-19 00:44:29 +0300

    add README.md

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
+from typing import Annotated
+from fastapi.responses import JSONResponse
 
 from utils import logger, get_db
 from celery_tasks import parse_quotes
@@ -22,13 +24,35 @@ async def run_quotes_parsing():
 
 
 @router.get("/quotes", response_model=list[schemas.QuoteResponseSchema])
-async def get_quotes_route():
+async def get_quotes_route(
+    author: Annotated[
+        str | None,
+        Query(description="Author of the quote."),
+    ] = None,
+    tags: Annotated[
+        list[str] | None,
+        Query(alias="tag", description="Tags of the quote."),
+    ] = None,
+):
     """Retrieve list of quotes."""
     _db = get_db()
     quotes_coll = _db.quotes
 
+    filter_cond = {}
+    if author:
+        filter_cond["author"] = author.strip()
+    if tags:
+        filter_cond["tags"] = {"$in": tags}
+
+    logger.info(f"Filtering quotes by conditions:\n{filter_cond}")
     result = []
-    async with quotes_coll.find() as cursor:
+    async with quotes_coll.find(filter_cond) as cursor:
         async for doc in cursor:
             result.append(schemas.QuoteResponseSchema(**doc))
-    return result if result else {"detail": "No quotes matching your filters!"}
+
+    if not result:
+        return JSONResponse(
+            {"detail": "No quotes matching your filters!"},
+            status_code=404,
+        )
+    return result
